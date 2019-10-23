4d0195ff2ca82e69e17c59788ef55069071d5864
Alvin Lo <p8c1b@ugrad.cs.ubc.ca>
2019-10-22 23:13:25 -0700

    Finished service, revamping feign client

--- src/main/java/com/butter/controller/ActivityController.java
+++ src/main/java/com/butter/controller/ActivityController.java
@@ -2,6 +2,9 @@ package com.butter.controller;
 
 import java.util.List;
 
+import com.butter.dto.model.ActivityDTO;
+import com.butter.service.ActivityService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ActivityController {
 
-    @RequestMapping(path = "/activities/{city}", method = RequestMethod.GET)
-    public List<ActivityDTO> discoverActivities(@PathVariable String city){
-        
+    @Autowired
+    private ActivityService activityService;
+
+    @RequestMapping(path = "/api/activities/?lat={lat}", method = RequestMethod.GET)
+    public List<ActivityDTO> discoverActivities(@PathVariable String lat, @PathVariable String lon){
+        return activityService.discoverActivities(lat, lon, "", "", "");
     }
 
+    @RequestMapping(path = "/api/activities/?lat={lat}&lon={lon}&category={category}", method = RequestMethod.GET)
+    public List<ActivityDTO> discoverActivities(@PathVariable String lat, @PathVariable String lon, @PathVariable String category){
+        return activityService.discoverActivities(lat, lon, category, "", "");
+    }
 
+    @RequestMapping(path = "/api/activities/?lat={lat}&lon={lon}&start={start}&end={end}", method = RequestMethod.GET)
+    public List<ActivityDTO> discoverActivities(@PathVariable String lat, @PathVariable String lon, @PathVariable String start, @PathVariable String end){
+        return activityService.discoverActivities(lat, lon, "", start, end);
+    }
+
+    @RequestMapping(path = "/api/activities/?lat={lat}&lon={lon}&category={category}&start={start}&end={end}", method = RequestMethod.GET)
+    public List<ActivityDTO> discoverActivities(@PathVariable String lat, @PathVariable String lon, @PathVariable String category, @PathVariable String start, @PathVariable String end){
+        return activityService.discoverActivities(lat, lon, category, start, end);
+    }
 
 }
--- src/main/java/com/butter/service/ActivityServiceImpl.java
+++ src/main/java/com/butter/service/ActivityServiceImpl.java
@@ -20,7 +20,9 @@ import com.butter.model.ticketmaster.TicketmasterResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ActivityServiceImpl implements ActivityService {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
